980dae00a5c3a7c7ad3cf079b0b4e39216a81955
Neo Chan <netkiller@msn.com>
2020-09-02 19:51:09 +0800

    init project

---
+++ src/main/java/cn/netkiller/microservice/App.java
@@ -0,0 +1,13 @@
+package cn.netkiller.microservice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
