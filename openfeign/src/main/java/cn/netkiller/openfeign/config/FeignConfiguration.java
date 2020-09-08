dd3940f02e58dc9df6a538fe6cc731143e8e1bb8
Neo Chen <netkiller@msn.com>
2020-09-08 13:14:49 +0800

    HTTP/2 SSL

---
+++ openfeign/src/main/java/cn/netkiller/openfeign/config/FeignConfiguration.java
@@ -0,0 +1,60 @@
+package cn.netkiller.openfeign.config;
+
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import org.springframework.cloud.netflix.ribbon.SpringClientFactory;
+import org.springframework.cloud.openfeign.ribbon.CachingSpringLoadBalancerFactory;
+import org.springframework.cloud.openfeign.ribbon.LoadBalancerFeignClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import feign.Client;
+
+@Configuration
+public class FeignConfiguration {
+	@Bean
+//	public Client feignClient() throws NoSuchAlgorithmException {
+//		SSLContext sslContext = SSLContext.getDefault();
+//		SSLSocketFactory sslContextFactory = sslContext.getSocketFactory();
+//		Client trustSSLSockets = new Client.Default(sslContextFactory, new NoopHostnameVerifier());
+//
+//		return trustSSLSockets;
+//	}
+
+	public Client feignClient(CachingSpringLoadBalancerFactory cachingFactory, SpringClientFactory clientFactory)
+			throws NoSuchAlgorithmException, KeyManagementException {
+		SSLContext ctx = SSLContext.getInstance("SSL");
+		X509TrustManager tm = new X509TrustManager() {
+			@Override
+			public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+			}
+
+			@Override
+			public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+			}
+
+			@Override
+			public X509Certificate[] getAcceptedIssuers() {
+				return null;
+			}
+		};
+		ctx.init(null, new TrustManager[] { tm }, null);
+		return new LoadBalancerFeignClient(new Client.Default(ctx.getSocketFactory(), new HostnameVerifier() {
+
+			@Override
+			public boolean verify(String hostname, SSLSession session) {
+				// TODO Auto-generated method stub
+				return true;
+			}
+		}), cachingFactory, clientFactory);
+	}
+}
