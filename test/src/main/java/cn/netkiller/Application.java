8c30ff4cfa8d76fc7bd321d962f1fc8f15a75f37
neo <netkiller@msn.com>
2021-07-29 20:37:30 +0800

    update

--- test/src/main/java/cn/netkiller/Application.java
+++ test/src/main/java/cn/netkiller/Application.java
@@ -2,14 +2,18 @@ package cn.netkiller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
-
 public class Application {
 
 	public static void main(String[] args) {
 
 		System.out.println("Starting...");
-		SpringApplication.run(Application.class, args);
+		// SpringApplication.run(Application.class, args);
+
+		SpringApplication springApplication = new SpringApplication(Application.class);
+		springApplication.addListeners(new ApplicationPidFileWriter());
+		springApplication.run(args);
 	}
 }
