2ef2a897c749061f31c6b4651b5885df1ec46b35
neo <netkiller@msn.com>
2021-10-14 19:37:04 +0800

    MongoDB 多数据源

--- test/src/main/java/cn/netkiller/Application.java
+++ test/src/main/java/cn/netkiller/Application.java
@@ -2,9 +2,11 @@ package cn.netkiller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.context.ApplicationPidFileWriter;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { MongoAutoConfiguration.class, MongoDataAutoConfiguration.class })
 public class Application {
 
 	public static void main(String[] args) {
